648cea5e82fce80c514014c9d3b05f0bb391b914
vineet <vineet8.4e8@gmail.com>
2025-04-24 18:34:17 +0530

    React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CCReact from "./Chai"
+function App() {
+  return (
+
+    <>
+   <CCReact></CCReact>
+   <h1>Hello</h1>
+   </>
+  )
+}
+
+export default App
---
+++ src/Chai.jsx
@@ -0,0 +1,7 @@
+function CCReact(){
+    return (
+        <h3>Welcome Mr. Escobar</h3>
+    )
+}
+
+export default CCReact
