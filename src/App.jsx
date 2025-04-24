cc2cb0b7d20378da0331a74fc4a38f56a83a671a
vineet <vineet8.4e8@gmail.com>
2025-04-24 18:36:33 +0530

    modifying

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,5 @@ function App() {
 }
 
 export default App
+
+// Always keep the names of components capitalized, whether you're working with Create React App or Vite. It's also better to keep the file names capitalized as well.
